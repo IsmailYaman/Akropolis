ad4bde234cc6ddbdc2602ec27fc29dad758bad84
IsmailYaman <iso_yaman@outlook.com>
2019-01-14 12:16:40 +0100

    reserveren begin

--- reserveringssysteem/reserveren1.php
+++ reserveringssysteem/reserveren1.php
@@ -15,6 +15,42 @@ require_once '../includes/form_validation.php';
 <body>
 <?php require_once '../includes_user/navbar.php'?>
 
+<div class="container">
+    <div class="card login">
+        <div class="card">
+            <h1>Reserveren</h1>
+         </div>
+        <h3 class="text-center">Stap 1 van 2</h3>
+        <div class="login-form">
+            <img class="text-center" src="../img/logo_small.png" width="250" height="auto" alt="">
+            <div class="main-div">
+                <div class="panel">
+
+                </div>
+                <form action="<?=$_SERVER['PHP_SELF'] ?>" method="post" id="Login">
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="username" id="username" placeholder="Gebruikersnaam" required/>
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control" name="password" id="password" placeholder="Wachtwoord" required/>
+                    </div>
+                    <button type="submit" name="submit" value="submit" class="btn btn-primary">Login</button>
+                    <?php if (isset($message)) { ?>
+                        <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                            <strong><?= $message; ?></strong>
+                            <button type="button button-danger" class="close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                    <?php } ?>
+
+                </form>
+            </div>
+
+        </div>
+    </div>
+</div>
+
 
 <?php require_once '../includes/footer.php' ?>
 <?php require_once '../includes/bootstrap_script.php' ?>
