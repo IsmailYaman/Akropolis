edd0c35b36a3dc3b22cb9c1ad3b2a300d30da8ea
Ismail Yaman <iso_yaman@outlook.com>
2019-07-09 09:31:11 +0200

    Upload

--- includes/db.php
+++ includes/db.php
@@ -3,11 +3,6 @@ $host       = "localhost";
 $database   = "akropolis";
 $user       = "root";
 $password   = "";
-
-//$host       = "localhost";
-//$database   = "u54933p52213_akropolis";
-//$user       = "u54933p52213_akropolis";
-//$password   = "IsoYaman01!";
 // login gegevens van de database
 
 $db = mysqli_connect($host, $user, $password, $database)
